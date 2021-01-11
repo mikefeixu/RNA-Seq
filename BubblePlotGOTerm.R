bdea9c459d7f450645de7d1085ac2a89e83d4ed3
mikefeixu <mike.fei.xu@gmail.com>
2021-01-11 15:56:52 -0500

    Cleaned up the code and added steps to run pipeine.

--- BubblePlotGOTerm.R
+++ BubblePlotGOTerm.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/Fei/Dropbox\ (EinsteinMed)/Fei/ChIP")
+setwd("")
 directory <- getwd()
 
 GO<- read.csv("GO.tsv", sep = "\t", header = T)
@@ -12,11 +12,8 @@ ggplot(GO, aes(x = EnrichmentFactor, y = Term, label = "")) +
   geom_point(aes(size = Count, colour = LogTransform),alpha=.99) +
   scale_x_continuous(breaks = seq(0.05, 0.3, 0.05)) +
   scale_y_discrete(limits=rev(GO$Term)) +
-  #scale_colour_gradient(low = "deeppink", high = "yellow") +
   scale_colour_gradient(low = "blue", high = "red") +
-  #scale_colour_gradient(low = "white", high = "black") +
   labs(colour = "-log10(pvalue)", size = "Count") +
-  #ggtitle("Selected GO Terms") +
   labs(x = "Enrichment Factor", y = "GO Terms") +
   geom_text(hjust = 3, size = 2) +
   scale_size(range = c(1,10)) +
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # RNA-Seq
+
+1. STAR Pipeline
+a. Run STAR_pipeline.sh on HPC environment
+b. Run deseq2STAR.R
+
+2. Tophat_pipeline.sh
+a. Run Tophat_pipeline.sh
+b. Run deseq2Tophat.R
+
+3. Data Visualization
+a. Run BubblePlotGoTerm.R if GO_selected.xlsx is exported from DAVID
