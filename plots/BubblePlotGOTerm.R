fb6953040831e2c6b654d657edd0dba56fc05e2d
Fei Xu <mike.fei.xu@gmail.com>
2021-04-13 13:08:08 -0400

    Added instructions for running BubblePlotGOTerm.R

--- plots/BubblePlotGOTerm.R
+++ plots/BubblePlotGOTerm.R
@@ -1,21 +1,25 @@
-setwd("")
-directory <- getwd()
+# Adjust the commented lines to tune the plots to an acceptable outlook.
+# Set project folder
+# Copy Selected_GO.tsv to GO folder, update with your selected GO Terms found from DAVID https://david.ncifcrf.gov/home.jsp
+library(ggplot2)
+setwd("Path to your project folder")
+selected_GO_terms <- "GO/Selected_GO.tsv"
 
-GO<- read.csv("GO.tsv", sep = "\t", header = T)
+GO <- read.csv("GO/Selected_GO.tsv", sep = "\t", header = T)
+GO$Term <- gsub("^GO(.*)~","",GO$Term)
 GO$EnrichmentFactor = GO$Count/GO$PopHits
 GO$LogTransform = (-1)*log10(GO$PValue)
-#GO <- GO[-18,]
 
-library(ggplot2)
-png(file = "GO.png", width = 2000, height = 1600, units = "px", res = 300)
+png(file = "GO_Up.png", width = 2000, height = 1600, units = "px", res = 300)
 ggplot(GO, aes(x = EnrichmentFactor, y = Term, label = "")) +
   geom_point(aes(size = Count, colour = LogTransform),alpha=.99) +
-  scale_x_continuous(breaks = seq(0.05, 0.3, 0.05)) +
+  #scale_x_continuous(breaks = seq(0.02, 0.02, 0.08)) +
   scale_y_discrete(limits=rev(GO$Term)) +
   scale_colour_gradient(low = "blue", high = "red") +
   labs(colour = "-log10(pvalue)", size = "Count") +
   labs(x = "Enrichment Factor", y = "GO Terms") +
   geom_text(hjust = 3, size = 2) +
   scale_size(range = c(1,10)) +
+  #xlim(0.03, 0.15) +
   theme_bw()
 dev.off()
