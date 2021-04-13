a786aae0841eda34f414c0a7968d49009b90cde8
Fei Xu <mike.fei.xu@gmail.com>
2021-04-13 15:45:21 -0400

    Update selected GO input filename

--- plots/BubblePlotGOTerm.R
+++ plots/BubblePlotGOTerm.R
@@ -10,16 +10,16 @@ GO$Term <- gsub("^GO(.*)~","",GO$Term)
 GO$EnrichmentFactor = GO$Count/GO$PopHits
 GO$LogTransform = (-1)*log10(GO$PValue)
 
-png(file = "GO_Up.png", width = 2000, height = 1600, units = "px", res = 300)
+png(file = "Selected_GO.png", width = 2000, height = 1600, units = "px", res = 300)
 ggplot(GO, aes(x = EnrichmentFactor, y = Term, label = "")) +
   geom_point(aes(size = Count, colour = LogTransform),alpha=.99) +
-  #scale_x_continuous(breaks = seq(0.02, 0.02, 0.08)) +
+  #scale_x_continuous(breaks = seq(0.00, 0.20, 0.02)) +
   scale_y_discrete(limits=rev(GO$Term)) +
   scale_colour_gradient(low = "blue", high = "red") +
   labs(colour = "-log10(pvalue)", size = "Count") +
   labs(x = "Enrichment Factor", y = "GO Terms") +
   geom_text(hjust = 3, size = 2) +
   scale_size(range = c(1,10)) +
-  #xlim(0.03, 0.15) +
+  #xlim(0.00, 0.15) +
   theme_bw()
 dev.off()
